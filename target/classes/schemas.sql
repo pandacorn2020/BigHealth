6c445704b019c09e895c19c77ee60220c2f66213
Jinsong Hu <jinsong>
2025-05-14 18:38:07 -0400

    fixes

--- target/classes/schemas.sql
+++
@@ -1,7 +0,0 @@
-create schema if not exists EBM;
-create schema if not exists CPM;
-create schema if not exists TCM;
-create schema if not exists IMM;
-create schema if not exists NUTR;
-create schema if not exists ORGAN;
-create schema if not exists AGING;
